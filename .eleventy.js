8d271fa8bac8059373e6d6a0135b1b41298e4768
aernoutmarkus-wq <aernoutmarkus@gmail.com>
2025-12-04 16:46:45 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,11 @@
 module.exports = function(eleventyConfig) {
+  // Zorg dat de admin-map 1-op-1 wordt gekopieerd
+  eleventyConfig.addPassthroughCopy("admin");
+
   return {
     dir: {
-      input: ".",
-      output: "_site"
+      input: ".",      // hier staan jouw bestanden, zoals Index.html :contentReference[oaicite:0]{index=0}
+      output: "_site"  // hier zet Eleventy de site neer voor Netlify
     }
   };
 };
